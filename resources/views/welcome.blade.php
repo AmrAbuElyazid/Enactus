78c1f06722f6623dfadbed54ecd546ce711f191a
Bellal <mbellal2000@gmail.com>
2017-03-12 02:53:16 +0200

    Some basic Layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,161 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}">
+<html lang="en" class="no-js">
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Blueprint: Split Layout</title>
+        <meta name="description" content="Blueprint: Split Layout" />
+        <meta name="keywords" content="website template, layout, css3, transition, effect, split, dual, two sides, portfolio" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="../favicon.ico">
+        <link rel="stylesheet" type="text/css" href="css/default.css" />
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/component2.css" />
+        <script src="js/modernizr.custom.js"></script>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
+        <div class="container">
+            <div id="splitlayout" class="splitlayout">
+                <div class="intro">
+                    <div class="side side-left">
+                        <div class="intro-content">
+                            <div class="profile"><img src="http://static.vectorcharacters.net/uploads/2013/02/Student_Vector_Character_Preview.jpg" alt="profile1"></div>
+                            <h1><span>Student</span><span>Start Learning</span></h1>
+                        </div>
+                        <div class="overlay"></div>
+                    </div>
+                    <div class="side side-right">
+                        <div class="intro-content">
+                            <div class="profile"><img src="http://www.okclipart.com/img103/skbrhjqiugvahxtujhxi.jpg" alt="profile2"></div>
+                            <h1><span>Teacher</span><span>Start Teaching</span></h1>
+                        </div>
+                        <div class="overlay"></div>
+                    </div>
                 </div>
-            @endif
+                <!-- /intro -->
+                <div class="page page-right">
+                    <div class="page-inner">
+                        <div class="main signin">
+                            <form class="cbp-mc-form" action="{{ route('teacher.login') }}" method="POST">
+                                {{ csrf_field() }}
+                                <div class="cbp-mc-column">                                   
+                                    <label for="email">Email Address</label>
+                                    <input type="email" id="email" name="email" placeholder="jon@doe.com">
+                                </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+                                <div class="cbp-mc-column">                                   
+                                    <label for="password">Password</label>
+                                    <input type="password" id="password" name="password" placeholder="**********">
+                                </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                                <div class="cbp-mc-submit-wrap"><input class="cbp-mc-submit" type="submit" value="Sign In"/></div>
+                            </form>
+                        </div>
+                        <div class="main signup">
+                            <form class="cbp-mc-form">
+                                <div class="cbp-mc-column">
+                                    <label for="first_name">First Name</label>
+                                    <input type="text" id="first_name" name="first_name" placeholder="Jonathan">
+                                    
+                                    <label for="last_name">Last Name</label>
+                                    <input type="text" id="last_name" name="last_name" placeholder="Doe">
+
+                                    <label for="email">Email Address</label>
+                                    <input type="email" id="email" name="email" placeholder="jon@doe.com">
+                                </div>
+
+                                <div class="cbp-mc-column">
+                                    <label for="phone">Phone Number</label>
+                                    <input type="text" id="phone" name="phone" placeholder="+351 999 999">
+
+                                    <label for="address">Address</label>
+                                    <input type="text" id="address" name="address" placeholder="Cairo, Egypt">
+                                </div>
+                                <div class="cbp-mc-column">
+                                    <label>What Is Your Talent</label>
+                                    <select id="talent" name="talent">
+                                        <option>Choose a talent</option>
+                                        <option>Ninja silence</option>
+                                        <option>Sumo power</option>
+                                        <option>Samurai precision</option>
+                                    </select>
+
+                                    <label for="comment">Leave A Comment</label>
+                                    <textarea id="comment" name="comment"></textarea>
+                                </div>
+                                <div class="cbp-mc-submit-wrap"><input class="cbp-mc-submit" type="submit" value="Sign Up"/></div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /page-inner -->
                 </div>
+                <!-- /page-right -->
+                <div class="page page-left">
+                    <div class="page-inner">
+                        <div class="main signin">
+                            <form class="cbp-mc-form" action="{{ route('student.login') }}" method="POST">
+                                {{ csrf_field() }}
+                                <div class="cbp-mc-column">                                   
+                                    <label for="">Email Address</label>
+                                    <input type="email" id="email" name="email" placeholder="jon@doe.com" required>
+
+                                </div>
+                                <div class="cbp-mc-column">                                   
+                                    <label for="">Password</label>
+                                    <input type="password" id="password" name="password" placeholder="**********" required>
+                                </div>
+
+                                <div class="cbp-mc-submit-wrap"><input class="cbp-mc-submit" type="submit" value="Sign In"/></div>
+                            </form>
+                        </div>
+                        <div class="main signup">
+                            <form class="cbp-mc-form" action="{{ route('student.register') }}" method="POST">
+                                {{ csrf_field() }}
+                                <div class="cbp-mc-column">
+                                    <label for="">First Name</label>
+                                    <input type="text" id="first_name" name="first_name" placeholder="Jonathan" required>
+                                    
+                                    <label for="">Password</label>
+                                    <input type="password" id="password" name="password" placeholder="**********" required>
+
+                                    <label for="phone_number">Phone Number</label>
+                                    <input type="text" id="phone_number" name="phone_number" placeholder="+351 999 999" required>
+                                </div>
+
+                                <div class="cbp-mc-column">
+                                    <label for="">Last Name</label>
+                                    <input type="text" id="last_name" name="last_name" placeholder="Doe" required>
+
+                                    <label for="password_confirmation">Confirm Password</label>
+                                    <input type="password" id="password_confirmation" name="password_confirmation" placeholder="**********" required>
+
+                                    <label for="">Address</label>
+                                    <input type="text" id="address" name="address" placeholder="Cairo, Egypt" required>
+
+                                </div>
+
+                                <div class="cbp-mc-column">
+                                    <label for="">Email Address</label>
+                                    <input type="email" id="email" name="email" placeholder="jon@doe.com" required>
+
+                                    <label for="date_of_birth">Date Of Birth</label>
+                                    <input type="date" id="date_of_birth" name="date_of_birth" required>
+                                </div>
+                                <div class="cbp-mc-submit-wrap"><input class="cbp-mc-submit" type="submit" value="Sign Up"/></div>
+                            </form>
+                        </div>
+                    </div>
+                    <!-- /page-inner -->
+                </div>
+                <!-- /page-left -->
+                <a href="#" class="back back-right" title="back to intro">&rarr;</a>
+                <a href="#" class="back back-left" title="back to intro">&larr;</a>
             </div>
+            <!-- /splitlayout -->
         </div>
+        <!-- /container -->
+        <script src="js/classie.js"></script>
+        <script src="js/cbpSplitLayout.js"></script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,34 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::group(['prefix' => 'teacher'], function () {
+    Route::post('/login', 'TeacherAuth\LoginController@login')->name('teacher.login');
+    
+    Route::post('/logout', 'TeacherAuth\LoginController@logout')->name('teacher.logout');
+
+    Route::post('/register', 'TeacherAuth\RegisterController@register')->name('teacher.register');
+
+    Route::post('/password/email', 'TeacherAuth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('/password/reset', 'TeacherAuth\ResetPasswordController@reset');
+    Route::get('/password/reset', 'TeacherAuth\ForgotPasswordController@showLinkRequestForm');
+    Route::get('/password/reset/{token}', 'TeacherAuth\ResetPasswordController@showResetForm');
+});
+
+Route::group(['prefix' => 'student'], function () {
+    Route::post('/login', 'StudentAuth\LoginController@login')->name('student.login');   
+
+    Route::post('/logout', 'StudentAuth\LoginController@logout')->name('student.logout');
+
+    Route::post('/register', 'StudentAuth\RegisterController@register')->name('student.register');
+
+    Route::post('/password/email', 'StudentAuth\ForgotPasswordController@sendResetLinkEmail');
+    Route::post('/password/reset', 'StudentAuth\ResetPasswordController@reset');
+    Route::get('/password/reset', 'StudentAuth\ForgotPasswordController@showLinkRequestForm');
+    Route::get('/password/reset/{token}', 'StudentAuth\ResetPasswordController@showResetForm');
 });
